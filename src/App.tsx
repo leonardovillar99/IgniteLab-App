43c737bac27fb1638d4459d226e2e6566ffdc4f3
Leonardo <l.fvillar@bol.com.br>
2022-06-27 16:34:17 -0300

    Meu App

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ApolloProvider } from "@apollo/client"
+import { Router } from "./Router"
+import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+  
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.png)'
+      },
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        green: {
+          300: '#80B37E',
+          500: '#80875F',
+          700: '#815F43',
+        },
+        blue: {
+          500: '#81D8F7',
+        },
+        orange: {
+          500: '#FBA94C',
+        },
+        red: {
+          500: '#F75A68',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
